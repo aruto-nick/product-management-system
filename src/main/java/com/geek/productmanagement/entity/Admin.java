51f21ae9a4e3cc70b67c716cbc85abc5d2b792ff
aruto-nick <effo.conti329@gmail.com>
2026-08-19 13:42:12 +0900

    Entity内のAdminクラス作成

--- src/main/java/com/geek/productmanagement/entity/Admin.java
+++ src/main/java/com/geek/productmanagement/entity/Admin.java
@@ -1,5 +1,31 @@
 package com.geek.productmanagement.entity;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class Admin {
 
+	private Integer id;
+	
+	private Integer storeId;
+	
+	private Integer positionId;
+	
+	private Integer authorityId;
+	
+	private String lastName;
+	
+	private String firstName;
+	
+	private String email;
+	
+	private String phoneNumber;
+	
+	private String password;
+	
+	private LocalDateTime createdAt;
+	
+	private LocalDateTime updatedAt;
 }
