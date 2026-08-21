1ffa7d89a1c4943fcd9c17e0fba95e07d4fab914
aruto-nick <effo.conti329@gmail.com>
2026-08-21 11:16:25 +0900

    Controller作成＆ログイン画面「タイトル」のみ記述

---
+++ src/main/java/com/geek/productmanagement/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.geek.productmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	String showLogin() {
+		return "login";
+	}
+	
+
+}
