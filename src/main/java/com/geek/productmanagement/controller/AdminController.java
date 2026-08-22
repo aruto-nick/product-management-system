1ad905c2f68cab76215325dc97fcfd40b80365d4
aruto-nick <effo.conti329@gmail.com>
2026-08-22 12:56:10 +0900

    TOP画面から遷移：管理者登録・一覧画面

---
+++ src/main/java/com/geek/productmanagement/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.geek.productmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	
+	//管理者登録画面に遷移
+	@GetMapping("/admin-register")
+	String showAdminRegister() {
+		return "admin-register";
+	}
+
+	//管理者一覧画面に遷移
+	@GetMapping("/admin-list")
+	String showAdminList() {
+		return "/admin-list";
+	}
+}
