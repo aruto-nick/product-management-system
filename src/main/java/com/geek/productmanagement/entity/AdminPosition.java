f387b51a13aced4796eba7dde27e65d63999e00c
aruto-nick <effo.conti329@gmail.com>
2026-08-24 10:31:54 +0900

    Entity作成：管理者登録画面用

---
+++ src/main/java/com/geek/productmanagement/entity/AdminPosition.java
@@ -0,0 +1,11 @@
+package com.geek.productmanagement.entity;
+
+import lombok.Data;
+
+@Data
+public class AdminPosition {
+	private Integer id;
+	
+	private String positionName;
+
+}
---
+++ src/main/java/com/geek/productmanagement/entity/Store.java
@@ -0,0 +1,13 @@
+package com.geek.productmanagement.entity;
+
+import lombok.Data;
+
+@Data
+public class Store {
+	private Integer id;
+	
+	private String storeName;
+	
+	private String address;
+
+}
