ecaa2cb4de6f9e28c451d83a9bad742a802840b8
aruto-nick <effo.conti329@gmail.com>
2026-08-25 11:08:35 +0900

    AdminControllerクラス：Serviceクラスで取得したデータをModelに格納

--- src/main/java/com/geek/productmanagement/controller/AdminController.java
+++ src/main/java/com/geek/productmanagement/controller/AdminController.java
@@ -1,21 +1,41 @@
 package com.geek.productmanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.geek.productmanagement.service.AdminAuthorityService;
+import com.geek.productmanagement.service.AdminPositionService;
+import com.geek.productmanagement.service.StoreService;
 
 @Controller
 public class AdminController {
 	
+	private final AdminAuthorityService adminAuthorityService;	
+	private final AdminPositionService adminPositionService;
+	private final StoreService storeService;
+	public AdminController(AdminAuthorityService adminAuthorityService, 
+							AdminPositionService adminPositionService, 
+							StoreService storeService) {
+		this.adminAuthorityService = adminAuthorityService;
+		this.adminPositionService = adminPositionService;
+		this.storeService = storeService;
+	}
+	
 	//管理者登録画面に遷移
 	@GetMapping("/admin-register")
-	String showAdminRegister() {
+	String showAdminRegister(Model model) {
+		//Serviceから取得したデータをModelに入れる
+		model.addAttribute("authorities", adminAuthorityService.findAll());
+		model.addAttribute("positions", adminPositionService.findAll());
+		model.addAttribute("stores", storeService.findAll());
 		return "admin-register";
 	}
 
 	//管理者一覧画面に遷移
 	@GetMapping("/admin-list")
 	String showAdminList() {
-		return "/admin-list";
+		return "admin-list";
 	}
 	
 
