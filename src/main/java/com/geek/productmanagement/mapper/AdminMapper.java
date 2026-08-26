7f2503b8b89a682c4fb40445bd58b378e3547aeb
aruto-nick <effo.conti329@gmail.com>
2026-08-26 11:07:41 +0900

    管理者詳細画面：html仮,DTO,Mapper,xml

--- src/main/java/com/geek/productmanagement/mapper/AdminMapper.java
+++ src/main/java/com/geek/productmanagement/mapper/AdminMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.geek.productmanagement.dto.AdminDetailDto;
 import com.geek.productmanagement.dto.AdminListDto;
 import com.geek.productmanagement.entity.Admin;
 
@@ -14,6 +15,9 @@ public interface AdminMapper {
 	//1人分の管理者情報を受け取り、DBにINSERTするメソッド
 	int insert(Admin admin);
 	
-	//管理者一覧画面のデータを復習取得して、返すメソッド
+	//管理者一覧画面のデータを複数件取得して、返すメソッド
 	List<AdminListDto> findAll();
+	
+	//管理者詳細画面のデータ１件取得して、返すメソッド
+	AdminDetailDto findDetailById(Integer id);
 }
