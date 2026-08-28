ce1cf6dba811499766dbbdbfeef38620f70ec1f3
aruto-nick <effo.conti329@gmail.com>
2026-08-28 12:43:41 +0900

    CustomUserDetailsServiceを実装：ログイン機能のため

---
+++ src/main/java/com/geek/productmanagement/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.geek.productmanagement.security;
+
+import org.springframework.context.annotation.Configuration;
+
+//Spring Securityの設定ファイル
+@Configuration
+public class SecurityConfig {
+
+}
