89835c24e4cd2ae0f4f4122458bc04b93f5b2d2b
aruto-nick <effo.conti329@gmail.com>
2026-09-24 14:45:28 +0900

    テストクラスの修正　※JUnitテスト実施＆成功確認済み

--- src/test/java/com/geek/productmanagement/mapper/AdminMapperTest.java
+++ src/test/java/com/geek/productmanagement/mapper/AdminMapperTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.geek.productmanagement.entity.Admin;
 
 @SpringBootTest
+//テストデータ登録＆テスト実施後、ロールバックしてテストデータを削除する
+@Transactional
 public class AdminMapperTest {
 
 	@Autowired
@@ -16,13 +19,32 @@ public class AdminMapperTest {
 	
 	@Test
 	void findByEmailTest(){
-		String email = "tanaka@gmail.com";
+		
+		//①テストデータの準備
+		Admin testAdmin = new Admin();
+		
+		//テスト用のデータを入れる
+		testAdmin.setStoreId(1);
+		testAdmin.setPositionId(1);
+		testAdmin.setAuthorityId(1);
+		testAdmin.setLastName("テスト");
+		testAdmin.setFirstName("A太朗");
+		testAdmin.setEmail("test@gmail.com");
+		testAdmin.setPhoneNumber("0120114514");
+		testAdmin.setPassword("testtest");
+		
+		//②テストデータをDBに登録
+		adminMapper.insert(testAdmin);
 	
-		Admin admin = adminMapper.findByEmail(email);
+		//③テスト対象のfindByEmailを実行
+		Admin admin = adminMapper.findByEmail(testAdmin.getEmail());
 	
+		//④結果の検証
+		//テストデータが存在するか否か
 		assertNotNull(admin);
 		
-		assertEquals(email, admin.getEmail());
+		//テストデータとDB取得データが一致するか否か
+		assertEquals(testAdmin.getEmail(), admin.getEmail());
 		
 		
 	}
